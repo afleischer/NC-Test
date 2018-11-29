7ce41bcf1f2dbdb253c461fb974c1038c6dd5781
Rex Beatie <rdev1163@outlook.com>
2018-11-29 14:40:52 -0800

    cant find the bundle?

--- webpack.config.js
+++ webpack.config.js
@@ -20,63 +20,7 @@ config = {
     },
     output: {
         filename: '[name].js',
-        paths: path.resolve(__dirname, './dist/')
-    },
-    resolve: {
-        // Add '.ts' and '.tsx' as a resolvable extension.
-        extensions:['.ts', '.tsx', '.js'],
-        alias: {
-
-        }
-    },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                    'style-loader',
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            minimize: true
-                        }
-                    },
-                    'sass-loader?sourceMap',
-                    "postcss-loader"
-                ]
-            },
-            {
-                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-                use: [{
-                    loader: 'file-loader',
-                    options: {
-                        name: '[name].[ext]',
-                        outputPath: 'fonts/'
-                    }
-                }]
-            },
-            {
-                test: /\.(png|jpg|jpeg)$/,
-                loader: 'url-loader'
-            },
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader'
-            }
-        ]
-    },
-    plugins: getPlugin()
-};
-
-
-config = {
-    entry: {
-        main: ['./src/main.ts']
-
-    },
-    output: {
-        filename: '[name].js',
-        paths: path.resolve(__dirname, './dist/')
+        path: path.resolve(__dirname, './dist/')
     },
     resolve: {
         // Add '.ts' and '.tsx' as a resolvable extension.
