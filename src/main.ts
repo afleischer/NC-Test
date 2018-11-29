b83c0b5561a320e01525a188c8274c0ce841db5b
Rex Beatie <rdev1163@outlook.com>
2018-11-29 01:01:50 -0800

    updating

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,3 @@
 import '../style/main.style.scss';
 
-import { NameClass } from './class/name-class';
-
-(function(){
-    
-    document.addEventListener('DOMContentLoaded', (event) => {
-        var instance = new NameClass();
-        instance.foo();
-    });
-
-})();
+console.log("Foo!");
